c04f5b709539c91091dcf600fcffc720d783cfc3
Brianca Knight <briancaknight@gmail.com>
2024-01-23 09:35:15 -0800

    add setter for package and pass test 3
    Co-authored-by: Trent Dietzel <dietzelbiz@outlook.com>

--- Parcels.Tests/ModelTests/ParcelTests.cs
+++ Parcels.Tests/ModelTests/ParcelTests.cs
@@ -23,5 +23,15 @@ namespace Parcel.Tests
       CollectionAssert.AreEqual(packageProperties, result);
 
     }
+
+    [TestMethod]
+    public void SetProperties_SetValuesOfProperties_Int()
+    {
+      Package newPackage = new Package(new int[] {10, 5, 2, 5});
+      int[] packageProperties = {15, 10, 4, 10};
+      newPackage.Properties = packageProperties;
+      int[] result = newPackage.Properties;
+      CollectionAssert.AreEqual(packageProperties, result);
+    }
   }
 }
--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -2,7 +2,7 @@ namespace Parcel.Models
 {
   public class Package
   {
-    public int[] Properties { get; }
+    public int[] Properties { get; set; }
     public Package(int[] properties)
     {
       Properties = properties;
